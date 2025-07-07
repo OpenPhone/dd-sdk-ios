60b6c9ed7e0b5070dae5e9bc9a3f9455d980dc6b
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2025-07-07 20:38:09 +0200

    RUM-10723 Add 1ns view filtering to RUMViewEventsFilter
    This prevents low-quality 1ns sessions that can occur when views receive no follow-up events after their initial 1ns placeholder duration (which represents failed eventual consistency scenario).

--- DatadogRUM/Sources/Feature/RUMFeature.swift
+++ DatadogRUM/Sources/Feature/RUMFeature.swift
@@ -205,7 +205,7 @@ internal final class RUMFeature: DatadogRemoteFeature {
         )
         self.requestBuilder = RequestBuilder(
             customIntakeURL: configuration.customEndpoint,
-            eventsFilter: RUMViewEventsFilter(),
+            eventsFilter: RUMViewEventsFilter(telemetry: core.telemetry),
             telemetry: core.telemetry
         )
         var messageReceivers: [FeatureMessageReceiver] = [
